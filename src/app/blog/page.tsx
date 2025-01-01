5bf695f74d90aae8c2e54928e4ed245ac8835a68
Toan Le <thanhtoanhcm1602@gmail.com>
2025-01-02 04:13:18 +0700

    Data-fetching-and-streaming with-suspense.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,13 @@
 import Posts from "@/app/ui/posts";
 import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function Page() {
   // Don't await the data fetching function
   // const posts = getPosts();
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <Posts />
     </Suspense>
   );
